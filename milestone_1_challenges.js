ae597c293415d8ddfabd8da4d76735f6ade62e3c
Stella-055 <stellaongayo243@gmail.com>
2025-05-31 11:29:48 +0300

    completed challenge1:sum of two numbers

---
+++ milestone_1_challenges.js
@@ -0,0 +1,6 @@
+"use strict";
+//Challenge 1:Return the Sum of Two Numbers
+function addition(num1, num2) {
+    return num1 + num2;
+}
+console.log(addition(4, 5)); // 9
---
+++ milestone_1_challenges.ts
@@ -0,0 +1,8 @@
+//Challenge 1:Return the Sum of Two Numbers
+
+function addition(num1:number,num2:number) : number{
+   
+    return num1+num2
+}
+
+console.log(addition(4,5))// 9
