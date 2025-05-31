a3ccdec85b31f57298f6072ddbd7385185cfc8c1
Stella-055 <stellaongayo243@gmail.com>
2025-05-31 19:37:28 +0300

    completed challenge1:sum of positives

---
+++ milestone_2_challenges.js
@@ -0,0 +1,11 @@
+//challenge 1:sum of positives
+function sumOfPositives(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+console.log(sumOfPositives([1, -4, 7, 12])); //20 
