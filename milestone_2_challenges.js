b62d100cbe8caec1f59f05a9ff15918949adbeb7
Stella-055 <stellaongayo243@gmail.com>
2025-05-31 19:39:44 +0300

    completed challenge2:find max value

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -9,3 +9,14 @@ function sumOfPositives(arr) {
     return sum;
 }
 console.log(sumOfPositives([1, -4, 7, 12])); //20 
+// challenge 2:Find Maximum Value
+function findMax(arr) {
+    var max = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log(findMax([1, 4, 7, 12])); //12 
--- milestone_2_challenges.ts
+++ milestone_2_challenges.ts
@@ -9,3 +9,18 @@ function sumOfPositives(arr:number[]): number {
     return sum;
 }
 console.log(sumOfPositives([1, -4, 7, 12]))//20 
+
+
+// challenge 2:Find Maximum Value
+
+function findMax(arr:number[]): number {
+    let max:number=arr[0] ;
+    for (let i=0; i < arr.length; i++){
+        if (arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log(findMax([1, 4, 7, 12]))//12 
+
